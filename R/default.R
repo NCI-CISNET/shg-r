562a765e1f0f04adc6a85a5c93ca3518a2f51c14
John Clarke <john-clarke@users.noreply.github.com>
2024-09-16 15:48:52 +0200

    Rename default.R to zzz.R and remove comments
    zzz.R is the standard filename

--- R/default.R
+++
@@ -1,4 +0,0 @@
-## For R 2.15.1 and later this also works. Note that calling loadModule() triggers
-## a load action, so this does not have to be placed in .onLoad() or evalqOnLoad().
-# I'm not sure why this needs to be in a file in R folder, but for now I'm leaving it hear.
-loadModule("SmokingSimulator", TRUE)
