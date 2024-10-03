7402e62788451c5ff720b01472207e1aa05839e4
John Clarke <john-clarke@users.noreply.github.com>
2024-10-03 20:20:45 +0200

    Add R-sandbox

---
+++ R-sandbox/TestRun.R
@@ -0,0 +1,52 @@
+#install.packages("devtools")
+#install.packages("Rcpp")
+#install.packages("rstream") # maybe need this later
+
+library(Rcpp)
+devtools::load_all()
+devtools::load_all()
+library(RcppSmokingHistoryGenerator)  # Package name
+shg <- new(SHGInterface) # Class
+
+rng1 <- new("rstream.mrg32k3a")
+rng2 <- rstream.clone(rng1)
+rstream.nextsubstream(rng2)
+rng3 <- rstream.clone(rng2)
+rstream.nextsubstream(rng3)
+rng4 <- rstream.clone(rng3)
+rstream.nextsubstream(rng4)
+
+shg$setRNGs(rng1, rng2, rng3, rng4)
+
+# test <- shg$runSim(10)
+# test2 <- shg$runSim(10) # should be different (continues with same streams)
+# shg$initialize() # resets seeds for all MT streams
+# shg$setRNGs(rng1, rng2, rng3, rng4)
+#
+# test5 <- shg$runSim(10) # should be same as first instance of test (old test for MT only)
+
+
+# devtools::load_all()
+#library(Rcpp)
+start_time <- Sys.time()
+test <- shg$runSim(10000)
+#10K with MRG = 4.26 secs
+#10K with MT = 0.426 secs
+
+#41.75??? secs with MT; 4.17 with MRG
+end_time <- Sys.time()
+end_time - start_time
+
+
+# Could also load libary and then access member functions
+#library(RcppSmokingHistoryGenerator)
+#smokinghistorygenerator()
+
+# Also works for
+# library(RcppExamples)
+# RcppRNGsExample(4)
+
+
+#https://www.iro.umontreal.ca/~lecuyer/myftp/papers/streams00.pdf
+#install.packages("rlecuyer")
+#install.packages("rstream")
--- R-sandbox/TestRunWithRstream.R
+++ R-sandbox/TestRunWithRstream.R
@@ -1,7 +1,7 @@
 library(Rcpp)
 library(rbenchmark)
 library(rstream)
-
+library(smokingHxGen)
 devtools::load_all() # recompiles the package if necessary
 devtools::load_all() # recompiles the package if necessary
 
@@ -18,11 +18,11 @@ rstream.nextsubstream(rng4)
 shg$initialize() # instantiates the Smoking_Simulator object in the interface
 
 shg$setRNGtype("MT") # Use internal (non-R) Mersenne Twister
-test <- shg$runSim(1000) # should be different (continues with same streams)
+Hx_MT <- shg$runSim(10^6) # should be different (continues with same streams)
 
 shg$setRNGs(rng1, rng2, rng3, rng4) # after initialize so that they can hang off the object
 shg$setRNGtype("rstream") # use MRG32k3a with substreams and batch generation
-test2 <- shg$runSim(1000) # should be same as first instance of test
+Hx_RSTREAM <- shg$runSim(1000) # should be same as first instance of test
 
 # TODO: investigate the following
 # NOTE: the first time the next segment is run, it can produce the following error:
@@ -30,23 +30,49 @@ test2 <- shg$runSim(1000) # should be same as first instance of test
 # But if you re-run the segment, it appears to run without any errors
 
 # Benchmark the different approaches
-n <- 10000
+N <- 10^6
 results <- benchmark(
-  SIM_MT = {
-    shg$setRNGtype("MT")
-    MT_SIM <- shg$runSim(n)
-  },
   SIM_RSTREAM = {
     shg$setRNGtype("rstream")
-    RSTREAM_SIM <- shg$runSim(n)
+    RSTREAM_SIM <- shg$runSim(N)
   },
+    SIM_MT = {
+    shg$setRNGtype("MT")
+    MT_SIM <- shg$runSim(N)
+  },
+  smokingHxGen = {
+    pop <-
+      list(
+        race = rep("white", N),
+        sex = sample(x = c("male", "female"), size = N, prob = c(0.5, 0.5), replace = TRUE),
+        birth_cohort = rep(1930:1949, N / 20),
+        some_other_var = stats::runif(N)
+      )
+
+    shg_bladder <- SmokingHistoryGenerator$new(
+      population = pop,
+      cause_of_death = "all_causes",
+      smoking_history_simulation_start_age = getOption("shg.min_smoking_initiation_age"), # 8
+      smoking_history_simulation_stop_age = getOption("shg.max_age"), # 110
+      lifetime_simulation_spawn_ages = 40,
+      lifetime_simulation_stop_ages = getOption("shg.max_age"),
+      specifications = getOption("shg.constants"), # coefficients
+      validate = TRUE
+     )
+    SHx <- shg_bladder$get_smoking_exposure_history(copy = FALSE)
+  },
+  # SHG_CLI = {
+  #   curdir = getwd()
+  #   setwd("/Users/jclarke/Documents/GitHub/smoking-history-generator/")
+  #   system("/Users/jclarke/Documents/GitHub/smoking-history-generator/lbc_smokehist_osx.exe test_input.txt")
+  #   setwd(curdir)
+  # },
   order = "relative",
-  replications = 2 # Number of times to run each expression
+  replications = 1 # Number of times to run each expression
 )
 
-results <- results[, c("test", "elapsed", "relative")]
-
 # Print out the benchmark results
+results <- results[, c("test", "elapsed", "relative")]
 print(results)
 
 # // Bonus: if we use rstream (MRG32k3a) we can parallelize the simulation (each segment assigned a thread)
@@ -58,3 +84,14 @@ print(results)
 
 # See https://www.appsilon.com/post/r-doparallel-dataframe
 # split by n/(cores-1) and clone + nextsubstream for each run
+
+
+#N=10^6
+#test elapsed relative
+#3 smokingHxGen  19.412    1.000
+#2       SIM_MT  42.759    2.203
+#1  SIM_RSTREAM  45.033    2.320
+
+# 10 cores with RSTREAM
+# 7.7 secs
+
