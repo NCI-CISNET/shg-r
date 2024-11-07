be92048391778148a46596fed4feaa66d2966718
John Clarke <john-clarke@users.noreply.github.com>
2024-11-07 16:30:05 +0100

    Remove commented code

--- src/wrapper.h
+++ src/wrapper.h
@@ -7,9 +7,7 @@ public:
     SHGInterface();
     Rcpp::DataFrame runSim(int repeat, short wRace, short wSex, short wYearBirth);
     void initialize();
-    //void setRNGs(SEXP rng1, SEXP rng2, SEXP rng3, SEXP rng4);
-    void RunWebVersion(const char *sInputFileName);
-    //void setRNGtype(std::string RNGtype);
+    void LegacyRunWebVersion(const char *sInputFileName);
     const char *sInputFile;
     const char *sOutputFile;
     Smoking_Simulator *pSimulator = 0;
